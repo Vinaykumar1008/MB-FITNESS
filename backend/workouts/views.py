bf0694ced86861eda2813d6545fb04ec3b5ad9d8
Vinaykumar1008 <v6744251@gmail.com>
2026-04-26 17:48:35 +0530

    feat:added my feature(short description)

--- backend/workouts/views.py
+++ backend/workouts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import WorkoutPlan
+from .serializers import WorkoutPlanSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_workouts(request):
+    workouts = WorkoutPlan.objects.all()
+    serializer = WorkoutPlanSerializer(workouts, many=True)
+    return Response(serializer.data)
