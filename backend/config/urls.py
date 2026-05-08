0b9957ce39433faee857425763e0e6d509c60b9a
Vinaykumar1008 <v6744251@gmail.com>
2026-05-08 14:34:34 +0530

    Added feedback  API and backend models

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/exercises/', include('exercises.urls')),
     path('api/workouts/', include('workouts.urls')),
+    path('api/feedbacks/', include('feedbacks.urls')),
 ]
--- backend/feedbacks/views.py
+++ backend/feedbacks/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Feedback
+from .serializers import FeedbackSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_feedbacks(request):
+    feedbacks = Feedback.objects.all()
+    serializer = FeedbackSerializer(feedbacks, many=True)
+    return Response(serializer.data)
