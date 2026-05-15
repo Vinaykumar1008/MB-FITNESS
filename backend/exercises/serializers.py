f2f90f8598045b35b1142b46def61e0f8ad96b2e
Vinaykumar1008 <v6744251@gmail.com>
2026-05-15 12:33:55 +0530

    added

--- backend/exercises/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Exercise
-
-class ExerciseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Exercise
-        fields = '__all__'
--- backend/exercises/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import get_exercises
-
-urlpatterns = [
-    path('', get_exercises),
-]
